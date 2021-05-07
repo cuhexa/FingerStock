2cf2ba95ac985f1bbc4f54bae1fa6eb9465b6e48
cuhexa <cuhexa@gmail.com>
2021-05-07 16:20:46 +0900

    자동로그인 및 계좌정보 불러오기 & 에러코드 등록

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -1,4 +1,6 @@
 from PyQt5.QAxContainer import *
+from PyQt5.QtCore import *
+from config.errorCode import *
 
 class Kiwoom(QAxWidget):
     def __init__(self):
@@ -6,10 +8,19 @@ class Kiwoom(QAxWidget):
 
         print("Kiwoom 클래스 입니다")
 
+        ##### eventloop 모음
+        self.login_event_loop = None
+        ###############################################
+
+        ##### 변수 모음
+        self.account_num = None
+        ###############################################
+
         self.get_ocx_insatance()
         self.event_slots()
 
         self.signal_login_commConnect()
+        self.get_account_info()
 
 
     def get_ocx_insatance(self):
@@ -18,8 +29,21 @@ class Kiwoom(QAxWidget):
     def event_slots(self):
         self.OnEventConnect.connect(self.login_slots)
 
-    def login_slots(self, errCode):
-        print(errCode)
-
     def signal_login_commConnect(self):
         self.dynamicCall('CommConnect()')
+
+        self.login_event_loop = QEventLoop()
+        self.login_event_loop.exec_()
+
+    def login_slots(self, errCode):
+        print(errCode)
+        print(errors(errCode))
+
+        self.login_event_loop.exit()
+
+    def get_account_info(self):
+        account_list = self.dynamicCall("GetLoginInfo(String)", "ACCNO")
+        # account_list = self.dynamicCall("GetLoginInfo(String)", "USER_ID")
+
+        self.account_num = account_list.split(';')[0]
+        print("내 계좌번호 %s" % self.account_num)  #8166270411
